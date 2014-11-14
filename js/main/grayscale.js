9b4f9e655fc265d8f41716fae8702ffaeaf94c2f
Carlos David Castro Felix <carlocastro@paypal.com>
2014-11-13 17:14:16 -0800

    Initial and current structure of Davixz.com

---
+++ js/main/grayscale.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+	$(window).scroll(function() {
+		if ($(".navbar").offset().top > 50) {
+			$(".navbar-fixed-top").addClass("top-nav-collapse");
+		} else {
+			$(".navbar-fixed-top").removeClass("top-nav-collapse");
+		}
+	});
+	
+	$(".nav a").on("click", function() {
+		if ($(".navbar-toggle").css("display") != "none") {
+			$(".navbar-toggle").trigger("click");
+		}
+	});
+	
+	$(".page-scroll a").bind("click", function(b) {
+		var a = $(this);
+		$("html, body").stop().animate({
+			scrollTop : $(a.attr("href")).offset().top
+		}, 1500, "easeInOutExpo");
+		b.preventDefault();
+	});
+});
